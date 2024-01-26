ecef0eb81d06fcd25d221932603eefecec988341
Ilias Thalassochoritis <elias.thal7@gmail.com>
2024-01-26 11:54:40 +0200

    chore(route.ts): remove console.log statements

--- app/api/pharmacies/[endpoint]/route.ts
+++ app/api/pharmacies/[endpoint]/route.ts
@@ -3,22 +3,13 @@ import { limiter } from "../../config/limiter";
 import { encrypt } from "@/app/api/utils/cryptoUtils";
 
 export async function GET(req: NextRequest) {
-  console.log("Received GET request");
-
   const url = req.nextUrl;
   const endpoint = url.pathname.split("/").pop();
   const params = Object.fromEntries(url.searchParams);
   const secret = process.env.API_SECRET || "";
   const remaining = await limiter.removeTokens(1);
   const origin = req.headers.get("origin");
-
-  console.log("Endpoint:", endpoint);
-  console.log("Params:", params);
-  console.log("Remaining tokens:", remaining);
-
   if (remaining < 1) {
-    console.log("Rate limit exceeded");
-
     const body = JSON.stringify({
       message: "Υπερβολικός αριθμός αιτημάτων, παρακαλώ δοκίμαστε σε λίγο",
     });
@@ -37,8 +28,6 @@ export async function GET(req: NextRequest) {
   const query = new URLSearchParams(params).toString();
   const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/${endpoint}?${query}`;
 
-  console.log("API URL:", apiUrl);
-
   try {
     const apiRes = await fetch(apiUrl, {
       method: "GET",
@@ -47,16 +36,12 @@ export async function GET(req: NextRequest) {
       },
     });
 
-    console.log("API Response Status:", apiRes.status);
-
     if (apiRes.status === 204) {
       return new NextResponse(null, { status: 204 });
     }
 
     if (!apiRes.ok) {
       const errorResponse = await apiRes.json();
-
-      console.log("API Error Response:", errorResponse);
 
       if (apiRes.status === 404) {
         return new NextResponse(
@@ -76,12 +61,9 @@ export async function GET(req: NextRequest) {
     }
 
     const data = await apiRes.json();
-    console.log("API Response Data:", data);
 
     const secretKey = process.env.NEXT_PUBLIC_CRYPTO_SECRET!;
     const encryptedData = encrypt(JSON.stringify(data), secretKey);
-
-    console.log("Encrypted Data:", encryptedData);
 
     return NextResponse.json(encryptedData);
   } catch (error) {
@@ -89,8 +71,6 @@ export async function GET(req: NextRequest) {
     if (error instanceof Error) {
       errorMessage = error.message;
     }
-
-    console.error("Error:", errorMessage);
 
     return new NextResponse(JSON.stringify({ message: errorMessage }), {
       status: 500,
