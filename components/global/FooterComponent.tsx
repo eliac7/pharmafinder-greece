970ea4bb1104ded1e79c6989c202c7b328adab3c
Ilias Thalassochoritis <elias.thal7@gmail.com>
2024-02-02 21:42:33 +0200

    style(FooterComponent.tsx): remove flex classes on the footer

--- components/global/FooterComponent.tsx
+++ components/global/FooterComponent.tsx
@@ -1,16 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto flex w-full flex-col items-center justify-center bg-transparent bg-white bg-opacity-40 bg-clip-padding p-2 text-center text-sm text-gray-700 backdrop-blur-lg backdrop-filter dark:bg-[#2f333d] dark:text-gray-300 md:flex-row">
-      <div className="hidden md:flex md:flex-1"></div>
-      <div className="flex-1 basis-1/2 text-center">
-        <span>
-          Δημιουργήθηκε με{" "}
-          <span role="img" aria-label="love">
-            ❤️
-          </span>{" "}
-          και λίγο Next.js, TypeScript, Tailwind CSS, Leaflet
-        </span>
-      </div>
+    <footer className="mx-auto w-full bg-transparent bg-white bg-opacity-40 bg-clip-padding p-2 text-center text-sm text-gray-700 backdrop-blur-lg backdrop-filter dark:bg-[#2f333d] dark:text-gray-300">
+      Δημιουργήθηκε με{" "}
+      <span role="img" aria-label="love">
+        ❤️
+      </span>{" "}
+      και λίγο Next.js, TypeScript, Tailwind CSS, Leaflet
     </footer>
   );
 }
