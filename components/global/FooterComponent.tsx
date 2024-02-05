17179603e58b045263924b3b4254c10630038d34
Ilias Thalassochoritis <elias.thal7@gmail.com>
2024-02-06 00:46:15 +0200

    feat(page.tsx/privacy-policy): Add page about privacy policy in greek. refactor(FooterComponent.tsx): Refactor the footer by adding the proper html tags alongside a link to the privacy policy page stlye(HeaderComponent.tsx): Fix some styling if we are in home page

--- components/global/FooterComponent.tsx
+++ components/global/FooterComponent.tsx
@@ -1,11 +1,26 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="mx-auto w-full bg-transparent bg-white bg-opacity-40 bg-clip-padding p-2 text-center text-sm text-gray-700 backdrop-blur-lg backdrop-filter dark:bg-[#2f333d] dark:text-gray-300">
-      Δημιουργήθηκε με{" "}
-      <span role="img" aria-label="love">
-        ❤️
-      </span>{" "}
-      και λίγο Next.js, TypeScript, Tailwind CSS, Leaflet
+      <div>
+        <span>
+          Δημιουργήθηκε με
+          <span role="img" aria-label="love" className="mx-1">
+            ❤️
+          </span>
+          και λίγο Next.js, TypeScript, Tailwind CSS, Leaflet
+        </span>
+      </div>
+      <div className="mt-3 flex items-center justify-center text-sm font-semibold leading-6 text-slate-700 dark:text-slate-400">
+        <Link
+          href="/privacy-policy"
+          passHref
+          className="hover:underline focus:underline"
+        >
+          Πολιτική Απορρήτου
+        </Link>
+      </div>
     </footer>
   );
 }
