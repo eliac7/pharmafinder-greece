4d83c041de5c6121283dfcc348c81034650571dc
Ilias Thalassochoritis <elias.thal7@gmail.com>
2024-02-17 23:31:44 +0200

    feat(FAQSection): Limit the FAQs to 3 and give the ability to user to see the rest of the FAQs by clicking the button chore(FAQ): Remove a question and rephrase one style(MapSection): Add some padding on the LinkButton

--- components/frontpage/FAQSection.tsx
+++ components/frontpage/FAQSection.tsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
 
 function FAQ() {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState<number>(0);
+  const [isShowFullFAQ, setIsShowFullFAQ] = useState<boolean>(false);
 
   const handleSelect = (index: number) => {
     if (selected === index) {
@@ -23,38 +24,46 @@ function FAQ() {
         </h1>
 
         <div className="mt-6 space-y-4 px-2 tablet:px-0">
-          {FaqData.map((item, index) => (
-            <div className="rounded-lg border-2 border-gray-700" key={index}>
-              <button
-                className="flex w-full items-center justify-between gap-x-2 p-4 focus-visible:outline-none"
-                onClick={() => handleSelect(index)}
-              >
-                <h1 className="stext-white text-left font-semibold">
-                  {item.question}
-                </h1>
+          {FaqData.slice(0, isShowFullFAQ ? FaqData.length : 3).map(
+            (item, index) => (
+              <div className="rounded-lg border-2 border-gray-700" key={index}>
+                <button
+                  className="flex w-full items-center justify-between gap-x-2 p-4 focus-visible:outline-none"
+                  onClick={() => handleSelect(index)}
+                >
+                  <h1 className="stext-white text-left font-semibold">
+                    {item.question}
+                  </h1>
 
-                <span className="rounded-full bg-gray-200 text-black">
-                  {selected === index ? (
-                    <CiCircleMinus size={30} />
-                  ) : (
-                    <CiCirclePlus size={30} />
+                  <span className="rounded-full bg-gray-200 text-black">
+                    {selected === index ? (
+                      <CiCircleMinus size={30} />
+                    ) : (
+                      <CiCirclePlus size={30} />
+                    )}
+                  </span>
+                </button>
+
+                <p
+                  className={clsx(
+                    "overflow-hidden text-left text-sm text-gray-300 transition-all duration-500 ease-in-out",
+                    {
+                      "h-0 opacity-0": selected !== index,
+                      "h-auto p-4 opacity-100": selected === index,
+                    },
                   )}
-                </span>
-              </button>
-
-              <p
-                className={clsx(
-                  "overflow-hidden text-left text-sm text-gray-300 transition-all duration-500 ease-in-out",
-                  {
-                    "h-0 opacity-0": selected !== index,
-                    "h-auto p-4 opacity-100": selected === index,
-                  },
-                )}
-                dangerouslySetInnerHTML={{ __html: item.answer }}
-              />
-            </div>
-          ))}
+                  dangerouslySetInnerHTML={{ __html: item.answer }}
+                />
+              </div>
+            ),
+          )}
         </div>
+        <button
+          onClick={() => setIsShowFullFAQ(!isShowFullFAQ)}
+          className="rounded-md bg-complementary-600 !p-4 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm transition-colors  hover:bg-complementary-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-complementary-200"
+        >
+          {isShowFullFAQ ? "Δείτε λιγότερες" : "Δείτε περισσότερες"}
+        </button>
       </div>
     </section>
   );
