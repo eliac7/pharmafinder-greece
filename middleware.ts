efde73e48b5697bc21d8b5fca37c11ea41885d53
Ilias Thalassochoritis <elias.thal7@gmail.com>
2024-02-18 19:35:26 +0200

    fix(middleware): Fix the return of NextResponse debug(vercel): Debug with console logging the country code and the country name

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,10 @@ export function middleware(request: NextRequest) {
   let countryCode = request.geo?.country ?? "";
   let countryName = request.geo?.region ?? "";
 
-  return new NextResponse(null, {
+  console.log("Country code:", countryCode);
+  console.log("Country name:", countryName);
+
+  return NextResponse.next({
     headers: {
       "x-country-code": countryCode,
       "x-country-name": countryName,
