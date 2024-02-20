4134843e7daefc15813ccff1e9423080b54171e2
Ilias Thalassochoritis <elias.thal7@gmail.com>
2024-02-20 19:56:27 +0200

    fix(mapUtils): Potential fix for not showing google titles by changing protocol from HTTP to HTTPS

--- utils/mapUtilts.ts
+++ utils/mapUtilts.ts
@@ -1,13 +1,13 @@
 export const getTileLayerUrl = (layerName: string) => {
   switch (layerName) {
     case "road":
-      return "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}";
+      return "https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}";
     case "satellite":
       return `https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png?api_key=${process.env.NEXT_PUBLIC_STADIA_API_KEY}`;
     case "dark":
       return `https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.NEXT_PUBLIC_STADIA_API_KEY}`;
     default:
-      return "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}";
+      return "https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}";
   }
 };
 
