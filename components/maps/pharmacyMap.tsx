af31947ac14c5b19076c72530ad99bcaa4da61ed
Ilias Thalassochoritis <elias.thal7@gmail.com>
2024-02-20 12:14:09 +0200

    feat: Export inside components into separate files from `pharmacyMap.tsx`
    Split inside components from pharmacyMap component into individual files for better organization and maintainability (RelocateUserButton,ToggleFilterButton,ToggleListButton)

--- components/maps/pharmacyMap.tsx
+++ components/maps/pharmacyMap.tsx
@@ -6,7 +6,7 @@ import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet/dist/leaflet.css";
 import { parseAsString, useQueryState } from "nuqs";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import {
   Circle,
   LayersControl,
@@ -22,13 +22,11 @@ import PharmacyMarker, { userLocationMarker } from "./PharmacyMarker";
 import { IMapProps, IPharmacyMapProps, IPoint } from "./types";
 
 import { useFilters } from "@/context/FiltersContext";
-import { ILocation } from "@/lib/interfaces";
-import { cn } from "@/lib/utils";
-import clsx from "clsx";
 import { useTheme } from "next-themes";
-import { FaFilter, FaLocationArrow } from "react-icons/fa";
 import TileLayerComponent from "./CustomTileLayer";
-import useFullscreenStatus from "@/hooks/useFullscreenStatus";
+import RelocateUserButton from "./buttons/RelocateUserButton";
+import ToggleFilterButton from "./buttons/ToggleFilterButton";
+import ToggleListButton from "./buttons/ToggleListButton";
 
 const POI: React.FC<IMapProps> = ({ selectedPharmacy }) => {
   const map = useMap();
@@ -47,187 +45,6 @@ const MapBoundsAdjuster = ({ points }: { points: IPoint[] }) => {
   }, [points, map]);
 
   return null;
-};
-
-const ToggleListButton = ({
-  toggleListVisibility,
-  isListVisible,
-  isHoveringHideButton,
-  setIsHoveringHideButton,
-}: {
-  toggleListVisibility: (isVisible: boolean) => void;
-  isListVisible: boolean;
-  isHoveringHideButton: boolean;
-  setIsHoveringHideButton: (value: boolean) => void;
-}) => {
-  const isFullscreen = useFullscreenStatus();
-  const map = useMap();
-
-  const handleClick = () => {
-    toggleListVisibility(!isListVisible);
-    setTimeout(() => {
-      map.invalidateSize();
-    }, 100);
-  };
-
-  if (isFullscreen) {
-    return null;
-  }
-
-  const handleMouseEnter = () => {
-    setIsHoveringHideButton(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHoveringHideButton(false);
-  };
-
-  const label = isListVisible ? "Απόκρυψη λίστας" : "Εμφάνιση λίστας";
-
-  return (
-    <button
-      className={cn(
-        "absolute left-2 top-1/2  z-[1000] hidden rounded-lg transition-all duration-500 md:block",
-      )}
-      onClick={handleClick}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      aria-label={label}
-      title={label}
-    >
-      <div className="relative flex h-[72px] w-8 items-center justify-center transition-opacity duration-100">
-        <div className="flex h-8 w-8 flex-col items-center justify-center rounded-full bg-complementary-400 text-center">
-          <div
-            className={clsx(
-              "translate-z-0 h-3 w-1 translate-y-[0.15rem] transform rounded-full bg-white transition-transform duration-300 dark:bg-white",
-              {
-                "!rotate-0": isHoveringHideButton && isListVisible,
-                "rotate-45": isListVisible,
-                "-rotate-45": !isListVisible,
-              },
-            )}
-          />
-          <div
-            className={clsx(
-              "translate-z-0 h-3 w-1 -translate-y-[0.15rem] transform rounded-full bg-white transition-transform duration-300 dark:bg-white",
-              {
-                "-rotate-45 ": isListVisible,
-                "rotate-0": isHoveringHideButton && isListVisible,
-                "rotate-45": !isListVisible,
-              },
-            )}
-          />
-        </div>
-        {isHoveringHideButton && (
-          <div className="shadow-xs absolute left-10 rounded-lg bg-gray-950 p-1 px-3 text-white">
-            <span className="flex items-center whitespace-pre-wrap px-2 py-1 text-center text-sm font-medium normal-case">
-              {label}
-            </span>
-            <span className="absolute -left-1 top-1/2 h-4 w-4 -translate-y-1/2 rotate-45 transform bg-gray-950"></span>
-          </div>
-        )}
-      </div>
-    </button>
-  );
-};
-
-const ReloacateButton = ({
-  getLocation,
-  location,
-}: {
-  getLocation: () => void;
-  location: Partial<ILocation>;
-}) => {
-  const map = useMap();
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-  const relocateUser = () => {
-    if (!location.latitude || !location.longitude) {
-      return;
-    }
-    map.setView([location.latitude, location.longitude], 16);
-    setIsPopupOpen(false);
-  };
-
-  return (
-    <div className="absolute right-3 top-28 z-[400] transition-all duration-300">
-      <button
-        className={cn(
-          "rounded-full bg-white p-2 shadow-md hover:bg-gray-100",
-          isPopupOpen && "bg-slate-500 hover:bg-slate-600",
-        )}
-        onClick={() => setIsPopupOpen(!isPopupOpen)}
-        aria-label="Επαναϋπολόγισε ή εστίασε σην τρέχουσα τοποθεσία"
-        title="Επαναϋπολόγισε ή εστίασε σην τρέχουσα τοποθεσία"
-      >
-        <FaLocationArrow size={16} color="black" />
-      </button>
-
-      <div
-        className={cn(
-          "invisible absolute right-0 top-10 z-[401] flex cursor-auto items-center  rounded-lg bg-gray-500 bg-opacity-50  text-xs font-semibold text-gray-700 opacity-0 backdrop-blur-sm backdrop-filter transition-all duration-300 dark:bg-primary-600",
-          isPopupOpen && "visible opacity-100",
-        )}
-      >
-        <ul className="flex list-none flex-col gap-1 p-2">
-          <li
-            className="cursor-pointer rounded-lg bg-primary-100 p-2 hover:bg-primary-300"
-            onClick={relocateUser}
-          >
-            Τρέχουσα
-          </li>
-          <li
-            className="cursor-pointer rounded-lg bg-primary-100 p-2 hover:bg-primary-300"
-            onClick={() => {
-              getLocation();
-              setIsPopupOpen(false);
-            }}
-          >
-            Επανατοποθέτηση
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const ToggleFilterButton = () => {
-  const isFullscreen = useFullscreenStatus();
-  const map = useMap();
-  const { isFilterOpen, setIsFilterOpen } = useFilters();
-
-  const toggleFilter = () => {
-    if (!isFullscreen) {
-      setIsFilterOpen(!isFilterOpen);
-      setTimeout(() => {
-        map.invalidateSize();
-      }, 100);
-    }
-  };
-
-  let label = isFilterOpen ? "Απόκρυψη φίλτρων" : "Εμφάνιση φίλτρων";
-  return (
-    <div
-      onClick={toggleFilter}
-      className={clsx(
-        "group absolute left-2 top-7 z-[1000] cursor-pointer rounded-lg bg-white p-2 shadow-md transition-all duration-300 hover:bg-complementary-400",
-        {
-          "!bg-complementary-400": isFilterOpen,
-          hidden: isFullscreen,
-        },
-      )}
-    >
-      <button className="block md:block" aria-label={label} title={label}>
-        <FaFilter size={20} color={isFilterOpen ? "white" : "black"} />
-      </button>
-      <div className="shadow-xs invisible absolute -top-2.5 left-12 rounded-lg bg-gray-950 p-1 px-3 text-white group-hover:visible">
-        <span className="flex items-center whitespace-pre-wrap px-2 py-1 text-center text-sm font-medium normal-case">
-          {label}
-        </span>
-        <span className="absolute -left-1 top-1/2 h-4 w-4 -translate-y-1/2 rotate-45 transform bg-gray-950"></span>
-      </div>
-    </div>
-  );
 };
 
 export default function PharmacyMap({
@@ -378,7 +195,7 @@ export default function PharmacyMap({
             </Popup>
           </Marker>
 
-          <ReloacateButton getLocation={getLocation} location={location} />
+          <RelocateUserButton getLocation={getLocation} location={location} />
         </>
       )}
 
